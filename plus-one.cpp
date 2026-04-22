8df5171fb154beb33329cd5b8d60863bf6862782
Ulises Martinez Araiza <mx.ulises@gmail.com>
2026-04-21 20:20:54 -0700

    Leetcode: https://leetcode.com/problems/plus-one/

---
+++ plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        digits[digits.size() - 1] += 1;
+        for (int i = digits.size() - 1; 0 < i; i--) {
+            digits[i - 1] += digits[i] / 10;
+            digits[i] %= 10;
+        }
+        if (digits[0] == 10) {
+            digits.insert(digits.begin(), 1);
+            digits[1] = 0;
+        }
+        return digits;
+    }
+};
