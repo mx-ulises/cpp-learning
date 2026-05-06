aa8fef5ee79a174686a40f947b4420b59f45d238
Ulises Martinez Araiza <mx.ulises@gmail.com>
2026-05-05 20:30:08 -0700

    Leetcode: https://leetcode.com/problems/reverse-bits/

---
+++ reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int reversed = 0;
+        for (int i = 0; i < 32; i++) {
+            reversed <<= 1;
+            reversed |= 1 & n;
+            n >>= 1;
+        }
+        return reversed;
+    }
+};
