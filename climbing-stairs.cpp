ac89489e924898eb08dd9fc0c25756d4de55cdde
Ulises Martinez Araiza <mx.ulises@gmail.com>
2026-07-16 19:34:37 -0700

    Leetcode: https://leetcode.com/problems/climbing-stairs/

---
+++ climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        int prev1 = 1, prev2 = 2, current = -1;
+        for (int i = 2; i < n; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return prev2;
+    }
+};
