659d9434cb869d960eb456a41728560e7db9784a
Ulises Martinez Araiza <mx.ulises@gmail.com>
2026-08-09 20:06:30 -0700

    Leetcode: https://leetcode.com/problems/maximum-gap/

---
+++ maximum-gap.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int maxGap = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            maxGap = max(maxGap, nums[i] - nums[i - 1]);
+        }
+        return maxGap;
+    }
+};
