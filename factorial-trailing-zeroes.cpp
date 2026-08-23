b7a894c06a9a30f60bfd75df68f619af768a16f3
Ulises Martinez Araiza <mx.ulises@gmail.com>
2026-08-22 20:18:07 -0700

    Fix empty files

---
+++ factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+        int divisor = 5;
+        while (divisor <= n) {
+            count += n / divisor;
+            divisor *= 5;
+        }
+        return count;
+    }
+};
